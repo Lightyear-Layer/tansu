69dd9ad1fea923043ae97eebca4bfd408964e37a
Steven van der Schoot <steven.vanderschoot@lightyear.one>
2026-04-23 13:56:11 +0200

    feat(schema,service): add transactional lake lifecycle interfaces
    Add TransactionRef/StageTransactionalRequest/FinalizeTransactionRequest and LakeHouse stage/finalize hooks with House dispatch.\n\nAlso expose EndTxn request handling through storage and broker service routing.

---
+++ tansu-storage/src/service/txn/end_txn.rs
@@ -0,0 +1,56 @@
+// Copyright ⓒ 2024-2026 Peter Morgan <peter.james.morgan@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use rama::{Context, Service};
+use tansu_sans_io::{ApiKey, EndTxnRequest, EndTxnResponse};
+use tracing::instrument;
+
+use crate::{Error, Result, Storage};
+
+/// A [`Service`] using [`Storage`] as [`Context`] taking [`EndTxnRequest`] returning [`EndTxnResponse`].
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct EndTxnService;
+
+impl ApiKey for EndTxnService {
+    const KEY: i16 = EndTxnRequest::KEY;
+}
+
+impl<G> Service<G, EndTxnRequest> for EndTxnService
+where
+    G: Storage,
+{
+    type Response = EndTxnResponse;
+    type Error = Error;
+
+    #[instrument(skip(ctx, req))]
+    async fn serve(
+        &self,
+        ctx: Context<G>,
+        req: EndTxnRequest,
+    ) -> Result<Self::Response, Self::Error> {
+        ctx.state()
+            .txn_end(
+                req.transactional_id.as_str(),
+                req.producer_id,
+                req.producer_epoch,
+                req.committed,
+            )
+            .await
+            .map(|error_code| {
+                EndTxnResponse::default()
+                    .throttle_time_ms(0)
+                    .error_code(error_code.into())
+            })
+    }
+}
